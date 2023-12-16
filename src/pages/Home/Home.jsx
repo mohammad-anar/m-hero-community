89980b624fd8fcaad7edb3a459b289b8f8e141ad
Mohammad Anar <ai.anarul4232@gmail.com>
2023-12-16 20:20:22 +0600

    building project section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
+import { Button } from "@mui/material";
 import Successgellary from "../../components/shared/Home/Successgellary";
 import Banner from "./Banner/Banner";
 import OurImpact from "./OurImpact/OurImpact";
+import WhatToLearn from "./WhatToLearn/WhatToLearn";
 import XFactor from "./xFactor/XFactor";
 
 const Home = () => {
@@ -26,122 +28,93 @@ const Home = () => {
         <XFactor />
       </div>
       {/* you will learn */}
-      <div className="mt-20">
-        <div className="max-w-[850px] mx-auto text-center">
-          <h2
-            id="gradientText"
-            className="text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-4"
-          >
-            What Will You Learn?_
-          </h2>
-          <p className="text-2xl">
-            The web course consists of 80+ modules, 25+ course projects, 15+
-            practice projects and 12 assignments.
-          </p>
-        </div>
-        <div className="flex items-center flex-wrap gap-12 justify-center mt-12">
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
+      <div className="my-20 mt-28">
+        <WhatToLearn />
+      </div>
+      {/* projects you build */}
+      <div>
+        <h2
+          id="gradientText"
+          className="text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-20"
+        >
+          Projects You Will Build_
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          
+          {/* card 1  */}
+          <div className="flex items-center justify-around gap-5 bg-slate-400 hover:bg-teal-600 duration-300 hover:scale-105 p-12 rounded-xl">
+            <div>
+              <h2 id="gradientText" className="text-5xl font-bold">Ranking <br/> Board</h2>
+            </div>
+            <div>
+              <img className="max-w-[250px] rounded-xl"
+                src="https://web.programming-hero.com/home/_next/image?url=https%3A%2F%2Fd3lhjpscbhcyrv.cloudfront.net%2Fproject%2FProject-2-Leaderboard.jpg&w=256&q=75"
+                alt=""
+              />
+            </div>
           </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              css
-            </h2>
+          {/* card 1  */}
+          <div className="flex items-center justify-around gap-5 bg-slate-400 hover:bg-teal-600 duration-300 hover:scale-105 p-12 rounded-xl">
+            <div>
+              <h2 id="gradientText" className="text-5xl font-bold">Shopping <br/> Cart</h2>
+            </div>
+            <div>
+              <img className="max-w-[250px] rounded-xl"
+                src="https://web.programming-hero.com/home/_next/image?url=https%3A%2F%2Fd3lhjpscbhcyrv.cloudfront.net%2Fproject%2FProject-2-Leaderboard.jpg&w=256&q=75"
+                alt=""
+              />
+            </div>
           </div>
-
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn.iconscout.com/icon/free/png-256/free-javascript-2038874-1720087.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              Js
-            </h2>
+          {/* card 1  */}
+          <div className="flex items-center justify-around gap-5 bg-slate-400 hover:bg-teal-600 duration-300 hover:scale-105 p-12 rounded-xl">
+            <div>
+              <h2 id="gradientText" className="text-5xl font-bold">Hot <br/> Gadgets</h2>
+            </div>
+            <div>
+              <img className="max-w-[250px] rounded-xl"
+                src="https://web.programming-hero.com/home/_next/image?url=https%3A%2F%2Fd3lhjpscbhcyrv.cloudfront.net%2Fproject%2FProject-2-Leaderboard.jpg&w=256&q=75"
+                alt=""
+              />
+            </div>
           </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
+          {/* card 1  */}
+          <div className="flex items-center justify-around gap-5 bg-slate-400 hover:bg-teal-600 duration-300 hover:scale-105 p-12 rounded-xl">
+            <div>
+              <h2 id="gradientText" className="text-5xl font-bold">Ema <br/> Jhon</h2>
+            </div>
+            <div>
+              <img className="max-w-[250px] rounded-xl"
+                src="https://web.programming-hero.com/home/_next/image?url=https%3A%2F%2Fd3lhjpscbhcyrv.cloudfront.net%2Fproject%2FProject-2-Leaderboard.jpg&w=256&q=75"
+                alt=""
+              />
+            </div>
           </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
+          {/* card 1  */}
+          <div className="flex items-center justify-around gap-5 bg-slate-400 hover:bg-teal-600 duration-300 hover:scale-105 p-12 rounded-xl">
+            <div>
+              <h2 id="gradientText" className="text-5xl font-bold">Genous <br/> Car</h2>
+            </div>
+            <div>
+              <img className="max-w-[250px] rounded-xl"
+                src="https://web.programming-hero.com/home/_next/image?url=https%3A%2F%2Fd3lhjpscbhcyrv.cloudfront.net%2Fproject%2FProject-2-Leaderboard.jpg&w=256&q=75"
+                alt=""
+              />
+            </div>
           </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
-          </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
-          </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee] inline-block rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
-          </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
-          </div>
-          <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
-            <img
-              className="w-[80px]"
-              src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-              alt="html"
-            />
-            <h2 className="text-xl font-bold text-white mt-2 text-center">
-              html
-            </h2>
+          {/* card 1  */}
+          <div className="flex items-center justify-around gap-5 bg-slate-400 hover:bg-teal-600 duration-300 hover:scale-105 p-12 rounded-xl">
+            <div>
+              <h2 id="gradientText" className="text-5xl font-bold">Doctors <br/> Portal</h2>
+            </div>
+            <div>
+              <img className="max-w-[250px] rounded-xl"
+                src="https://web.programming-hero.com/home/_next/image?url=https%3A%2F%2Fd3lhjpscbhcyrv.cloudfront.net%2Fproject%2FProject-2-Leaderboard.jpg&w=256&q=75"
+                alt=""
+              />
+            </div>
           </div>
         </div>
+        <Button  sx={{fontWeight:"600", color: "#561fee", border: "1px solid #561fee", textTransform:"capitalize", margin: "30px auto", display:"block"}} variant="outlined" size="large">All Projects</Button>
       </div>
     </div>
   );
---
+++ src/pages/Home/WhatToLearn/WhatToLearn.jsx
@@ -0,0 +1,129 @@
+const WhatToLearn = () => {
+  return (
+    <div className="mt-20">
+      <div className="max-w-[850px] mx-auto text-center">
+        <h2
+          id="gradientText"
+          className="text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-4"
+        >
+          What Will You Learn?_
+        </h2>
+        <p className="text-2xl">
+          The web course consists of 80+ modules, 25+ course projects, 15+
+          practice projects and 12 assignments.
+        </p>
+      </div>
+      <div className="flex items-center flex-wrap gap-12 justify-center mt-12">
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            html
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px] "
+            src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">css</h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr h-full from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px] h-[80px]"
+            src="https://static-00.iconduck.com/assets.00/tailwind-css-icon-2048x1229-u8dzt4uh.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            Tailwind
+          </h2>
+        </div>
+
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://cdn.iconscout.com/icon/free/png-256/free-javascript-2038874-1720087.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">Js</h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://icons.veryicon.com/png/o/business/vscode-program-item-icon/react-3.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            React
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://static-00.iconduck.com/assets.00/node-js-icon-1901x2048-mk1e13df.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            Node.js
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://cdn.iconscout.com/icon/free/png-256/free-express-8-1175029.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            Express.js
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://seeklogo.com/images/M/mongodb-logo-D13D67C930-seeklogo.com.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            MongoDB
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee] inline-block rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            Firebase
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            Git hub
+          </h2>
+        </div>
+        <div className="p-6 pt-10 pb-2 bg-gradient-to-tr from-[#ef4bf5] to-[#561fee]  rounded-2xl">
+          <img
+            className="w-[80px]"
+            src="https://static-00.iconduck.com/assets.00/next-js-icon-512x512-zuauazrk.png"
+            alt="html"
+          />
+          <h2 className="text-xl font-bold text-white mt-2 text-center">
+            Express.js
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhatToLearn;
